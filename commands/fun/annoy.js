4aba73b15a717ff5f130e5efdcead2976910ca56
Fırat Batar <firat.o.batar@gmail.com>
2023-11-18 11:48:54 +0300

    Add 'annoy' command Annoy a user with a random message

---
+++ commands/fun/annoy.js
@@ -0,0 +1,22 @@
+import { SlashCommandBuilder, userMention } from 'discord.js';
+
+const annoy_messages = [
+    "senin benin ben bir taraflarını mikim!",
+    "anan.",
+]
+
+export const data = new SlashCommandBuilder()
+    .setName('annoy')
+    .setDescription('Annoy a user with insults!')
+    .addUserOption(option => 
+        option.setName('target')
+            .setDescription('The user to annoy.')
+            .setRequired(true));
+
+export async function execute(interaction) {
+    const target = interaction.options.getUser('target');
+    
+    const random_message_idx = Math.floor(Math.random() * annoy_messages.length);
+    const annoy_msg = annoy_messages[random_message_idx];
+    await interaction.reply(`${userMention(target.id)} ${annoy_msg}`);
+}
