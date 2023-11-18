8dddcb45c8c27e64ea69280a47c90ceb9803a66a
Fırat Batar <firat.o.batar@gmail.com>
2023-11-18 11:23:15 +0300

    Seperate events Seperate events into different files similar to commands.

--- bot.js
+++ bot.js
@@ -39,31 +39,19 @@ for (const folder of commandFolders) {
     }
 }
 
+// Register events
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-// When the client is ready, run this code (only once)
-client.once(Events.ClientReady, readyDiscord);
-
-// Event listener for when a slash command is executed
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-    
-    const command = interaction.client.commands.get(interaction.commandName);
-    if (!command) {
-        console.error(`No command matching ${Interaction.commandName}`);
-        return;
-    }
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-    }
-})
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = await import(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 // Login to Discord with your client's token
 client.login(process.env.TOKEN);
